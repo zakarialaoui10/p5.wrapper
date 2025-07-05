02f69c19ba5084cc757b1157eb984672043e5c9c
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-07-05 11:51:57 +0100

    init ziko

---
+++ src/ziko/canvas/canvas2d.js
@@ -0,0 +1,14 @@
+import {ZikoUIElement} from "ziko"
+class ZikoP5Canvas2D extends ZikoUIElement{
+    constructor(){
+        super()
+    }
+    append(){
+
+    }
+}
+
+const P5Canvas2D = () => new ZikoP5Canvas2D();
+export{
+    P5Canvas2D
+}
