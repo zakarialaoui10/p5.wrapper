f3588d80aa8daf0dd776c7e6e7e96f1866ae2492
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-07-05 12:36:35 +0100

    setup P5Canvas2D

--- examples/ziko/src/main.js
+++ examples/ziko/src/main.js
@@ -1,7 +1,13 @@
 import {Flex} from "ziko"
 import Hello from "./components/Hello.js"
+import { P5Canvas2D, Circle } from "p5.wrapper/ziko"
+globalThis.c = P5Canvas2D(
+  Circle({x:50, y:50, r:20})
+)
+console.log(c.width)
 Flex(
-  Hello({name : "world"})
+  Hello({name : "world"}),
+  c
 ).size("100vw","100vh").vertical(0, 0).style({
   background : "darkblue",
   color : "gold",
--- src/ziko/canvas/canvas2d.js
+++ src/ziko/canvas/canvas2d.js
@@ -1,14 +1,46 @@
-import {ZikoUIElement} from "ziko"
-class ZikoP5Canvas2D extends ZikoUIElement{
-    constructor(){
-        super()
-    }
-    append(){
-
-    }
+import { ZikoUIElement } from "ziko";
+import p5  from "p5";
+class ZikoP5Canvas2D extends ZikoUIElement {
+  constructor(...children) {
+    super("div");
+    this.size("70%","150px")
+    this.style({
+        outline : "1px red solid",
+        
+    })
+    this.drawFns = [];
+    this.context = {
+      register: (fn) => this.drawFns.push(fn), 
+      unregister: (fn) => {
+        const index = this.drawFns.indexOf(fn);
+        if (index !== -1) this.drawFns.splice(index, 1);
+      },
+    };
+    this.p5 = new p5((p) => {
+      p.setup = () => {
+        p.createCanvas(this.width, this.height).parent(
+          this.element,
+        );
+      };
+      p.draw = () => {
+        p.clear();
+        this.drawFns.forEach((fn) => fn(p));
+      };
+    });
+    this.append(...children)
+  }
+  append(...items){
+    for (const item of items) (typeof item === "function") && item(this.context);
+    return this;
+  }
 }
 
-const P5Canvas2D = () => new ZikoP5Canvas2D();
-export{
-    P5Canvas2D
-}
+const P5Canvas2D = (...children) => new ZikoP5Canvas2D(...children);
+export const Circle = ({ x, y, r }) => (ctx) => {
+  const drawFn = (p) => p.ellipse(x, y, r * 2);
+  ctx.register(drawFn);
+
+  console.log(ctx)
+  return null;
+};
+export { ZikoP5Canvas2D, P5Canvas2D };
