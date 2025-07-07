a3cd621313b289acf746ff496748e6dc41cdf654
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-07-07 15:30:26 +0100

    setup vanjs example

--- examples/vanjs/src/App.js
+++ examples/vanjs/src/App.js
@@ -1,40 +1,43 @@
 import van from "vanjs-core";
 import Counter from "./components/Counter";
 import "./App.css";
-// import { text } from "ziko"
+import {cos, sin, PI } from "ziko"
+import Technologies from "./technologies.js";
 
 import { P5Canvas2D, P5Circle } from "p5.wrapper/van";
+const c1 = P5Circle(0, 0, 30) 
+const c = P5Canvas2D(
+  c1
+).size("100%")
+c.setCustomLoopCallback(e=>{
+  c1.posX(150+100*cos(e.iter*PI/50))
+  c1.posY(150+100*sin(e.iter*PI/50))
+})
 
-export const App = () => {
-  const { div, h1, img, p, a } = van.tags;
-  return div(
-    P5Canvas2D(
-      P5Circle(100, 100, 20)
-    ).size("100%","200px"),
-    a(
-      { href: "https://vite.dev", target: "_blank" },
+const { div, h5, img, p, a } = van.tags;
+const Tech = (Tech_Name) => {
+  const {icon, url} = Technologies[Tech_Name]
+  return a(
+      { href: url, target: "_blank" },
       img({
-        src: "/vite.svg",
+        src: icon,
         class: "logo",
-        alt: "Vite logo",
+        alt: `${Tech_Name} logo`,
         width: 96,
         height: 96,
       }),
+    )
+  }
+export const App = () => {
+  return div(
+    Tech("vanjs"),
+    Tech('vitejs'),
+    Tech('zikojs'),
+    Tech('p5js'),
+    p(
+      "Hello VanJS users! This is a p5 canvas powered by p5.wrapper, a wrapper built on top of ZikoJS",
     ),
-    a(
-      { href: "https://vanjs.org", target: "_blank" },
-      img({
-        src: "/vanjs.svg",
-        class: "logo vanjs",
-        alt: "VanJS logo",
-        width: 96,
-        height: 96,
-      }),
-    ),
-    h1(
-      "Hello VanJS!",
-    ),
-    div({ class: "card" }, Counter()),
+    c,
     p({ class: "read-the-docs" }, "Click on the VanJS logo to learn more"),
   );
 };
---
+++ examples/vanjs/src/technologies.js
@@ -0,0 +1,19 @@
+const Technologies = {
+    zikojs : {
+        icon : "/zikojs.svg",
+        url : "https://github.com/zakarialaoui10/ziko.js"
+    },
+    vanjs : {
+        icon : "/vanjs.svg",
+        url : "https://vanjs.org",
+    },
+    vitejs : {
+        icon : "/vite.svg",
+        url : "https://vite.dev",
+    },
+    p5js : {
+        icon : "/p5.png",
+        url : "https://p5js.org/"
+    }
+}
+export default Technologies;
