3befb7753fc77d096287c3612d68db34a2671e20
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-07-17 19:24:44 +0100

    add square

--- wrappers/ziko/components/2d/index.js
+++ wrappers/ziko/components/2d/index.js
@@ -2,3 +2,5 @@ export * from "./circle.js";
 export * from "./line.js";
 export * from "./ellipse.js";
 export * from "./arc.js";
+export * from "./point.js";
+export * from "./square.js";
---
+++ wrappers/ziko/components/2d/square.js
@@ -0,0 +1,32 @@
+import { ZikoP5Shape2D } from "./p5shape2d.js"
+
+class ZikoP5Square extends ZikoP5Shape2D{
+  constructor(x, y, s, tr, tl, br, bl){
+    super(x, y)
+    Object.assign(this.cache.geometry,{
+      s,
+      tr, 
+      tl, 
+      br, 
+      bl
+    })
+  }
+  __draw_geo__(p){
+      p.square(
+        this.cache.geometry.x,
+        this.cache.geometry.y,
+        this.cache.geometry.s,
+        this.cache.geometry.tr,
+        this.cache.geometry.tl,
+        this.cache.geometry.br,
+        this.cache.geometry.bl,
+      );
+  }
+}
+const square = (x, y, s, {tr, tl, br, bl} = {}) => new ZikoP5Square(x, y, s, tr, tl, br, bl);
+const Square = ({x, y, r} = {}) => new ZikoP5Square(x, y, r);
+export{
+  ZikoP5Square,
+  square,
+  Square
+}
