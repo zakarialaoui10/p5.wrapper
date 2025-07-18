222951c5201e47f566c5c08f955cc77d72b564d6
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-07-18 09:38:41 +0100

    add .npmignore

--- examples/ziko/src/main.js
+++ examples/ziko/src/main.js
@@ -1,21 +1,27 @@
 import {Flex, cos, sin, PI} from "ziko"
 import Hello from "./components/Hello.js"
-import { P5Canvas2D, circle, line, triangle } from "p5.wrapper/ziko"
-globalThis.circle = circle
-globalThis.c1 = circle(0, 0, 20) 
-globalThis.l1 = line(-50, -50, 50, -50)
-globalThis.c = P5Canvas2D(
-  c1,
-  l1,
-  // triangle(-50, -50, 50, -50, 0, 100)
-).view(-150, -150, 150, 150)
-// c.setCustomLoopCallback(e=>{
-//   c1.posX(100*cos(e.iter*PI/50))
-//   c1.posY(100*sin(e.iter*PI/50))
-//   // l1.posY(50* sin(e.iter *PI/20), 50* sin(e.iter *PI/20))
-//   // l1.translateY(e.iter/10)
-// })
+import { Canvas2D, Canvas3D, box, circle, line, triangle } from "p5.wrapper/ziko"
 
+import p5 from "p5"
+
+// globalThis.v = new p5.Vector(0, 10, 100)
+// globalThis.c1 = circle(0, 0, 20) 
+// globalThis.l1 = line(-50, -50, 50, -50)
+// globalThis.c = Canvas2D(
+//   c1,
+//   l1,
+//   // triangle(-50, -50, 50, -50, 0, 100)
+// ).view(-150, -150, 150, 150)
+// // c.setCustomLoopCallback(e=>{
+// //   c1.posX(100*cos(e.iter*PI/50))
+// //   c1.posY(100*sin(e.iter*PI/50))
+// //   // l1.posY(50* sin(e.iter *PI/20), 50* sin(e.iter *PI/20))
+// //   // l1.translateY(e.iter/10)
+// // })
+
+globalThis.c = Canvas3D(
+  box(10, 0, 0, 200, 200, 200)
+).useOrbitControls()
 Flex(
   Hello({name : "world"}),
   c
--- wrappers/ziko/components/3d/box.js
+++ wrappers/ziko/components/3d/box.js
@@ -1,19 +1,21 @@
 import { ZikoP5Shape3D } from "./p5shape3d.js";
+import p5 from "p5";
 
 class ZikoP5Box extends ZikoP5Shape3D{
   constructor(x, y, z, w, h, d){
-    super(x, y, z)
-    Object.assign(this.cache,{
+    super([x, y, z])
+    Object.assign(this.cache.geometry,{
       w,
       h, 
       d
     })
   }
   draw(p){
-    p.push();
-    p.translate(this.cache.x, this.cache.y, this.cache.z);
-    p.box(this.cache.w, this.cache.h, this.cache.d);
-    p.pop()
+    const [x, y, z] = this.cache.geometry.coordinates;
+    const {w, h, d} = this.cache.geometry
+    // p.translate(x, y, z);
+    p.box(w, h, d);
+    // p.pop()
   }
 }
 const box = (x, y, z, w, h, d) => new ZikoP5Box(x, y, z, w, h, d);
